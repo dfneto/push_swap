ed94d8390a6cae620c339c1804174769acfc12fb
davifern <pdavidfneto@gmail.com>
2023-07-24 20:08:21 +0200

    adding todo

--- sort_algorithm.c
+++ sort_algorithm.c
@@ -1,6 +1,7 @@
 #include "push_swap.h"
 
 
+//TODO: façø o bubble sorte e depois outro algorito que use as duas pilhas para entender como funciona o uso dos movimentos e para usar os programas de gráficos e de ver em quanto tempo meu algoritmo resolve o problema. Depois estudar um pouco sobre complexidade
 void	bubble_sort(t_list **first, int len)
 {
 	t_list	*a;

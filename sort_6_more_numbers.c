a765ed250984734a7a13de77b43997e55fff425a
davifern <pdavidfneto@gmail.com>
2023-08-09 16:39:00 +0200

    refactored

--- sort_6_more_numbers.c
+++ sort_6_more_numbers.c
@@ -80,12 +80,29 @@ void	push_all_nodes_to_b_by_chunks(t_list **first, t_list **first_b)
 	}
 }
 
+int	calculate_index_distance(t_list *root, int index_desired, int len)
+{
+	int	index_distance;
+
+	index_distance = 0;
+	while(index_distance < len)
+	{
+		if(root->index == index_desired)
+			return (index_distance);
+		else
+			root = root->next;
+		index_distance++;
+	}
+	return (index_distance);
+}
+
 void	sort_6_more_numbers(char *argv[], int len)
 {
 	t_list *first = create_node(atoi(argv[1]));
 	t_list *first_b = NULL;
 	t_list *node;
 	int	index_desired;
+	int	index_distance = 0;
 
 	int i = 2;
 	while(i <= len)
@@ -99,7 +116,7 @@ void	sort_6_more_numbers(char *argv[], int len)
 
 	push_all_nodes_to_b_by_chunks(&first, &first_b);
 
-	index_desired = --len; //começa do maior ao menor
+	index_desired = len - 1; //começa do maior ao menor
 	while(first_b)
 	{	//se o topo da stack é o indice que procuro faco push
 		if(first_b->index == index_desired)
@@ -111,57 +128,23 @@ void	sort_6_more_numbers(char *argv[], int len)
 				index_desired--;	
 			}
 			index_desired--;
-			// continue;
+			index_distance = 0;
 		}//TODO: acrescentar no if abaixo se o no atual eh o indice_desired -2 tbm
 		else if (first_b->index == index_desired - 1) //se o top da stack é o índice anterior que procuro envio para a stack A 
-		{
 			push(&first_b, &first, 'a'); //se eu fiz um push de um número menor, o próximo index_desired não vai estar em B, mas em A
-			// continue;
-		}
-		else //se o top não é o indice, vou procurar o caminho mais curto para fazer o rotate ou reverse-rotate
+		else
 		{
-			//TODO: extrair a parte de contar o i em uma função a parte
-			i = 0; //i vai me dizer quantos nós têm acima do nó com o index procurado
-			node = first_b;
-			while(i <= index_desired) //TODO: verificar se node eh nulo em todos os casos que uso node e que nao fiz isso antes (para evitar repetir codigo)										
-			{
-				if(node && node->index == index_desired) //se o no tem o índice procurado
-				{
-					node = first_b; //TODO: substituir node por first_b
-					if (i <= index_desired / 2) //faco rotate ate encontrar o elemento e deixar ele no topo da stack de b. E se encontro o len-1 faco um push
-					{
-						while(node && node->index != index_desired)//o nó não eh o elemento que procuro
-						{
-							if (node->index == (index_desired - 1))  //acrescentar no if abaixo se o no atual eh o indice_desired -2 tbm
-								push(&first_b, &first, 'a');
-							node = node->next;
-							rotate(&first_b, 'b');
-						}
-					}
-					else //faco rr ate encontrar o elemento e deixar ele no topo da stack de b. E se encontro o len-1 faco um push
-					{
-						while(node->index != index_desired)//o nó não eh o elemento que procuro
-						{	
-							if (node->index == index_desired - 1) //acrescentar no if abaixo se o no atual eh o indice_desired -2 tbm
-								push(&first_b, &first, 'a');
-							reverse_rotate(&first_b, 'b');
-							node = first_b;
-						}
-					}
-					break; //sair deste while
-				}
-				else //se o no não tem o índice procurado
-				{
-					if (node)
-						node = node->next; //var para o próximo nó
-					i++;
-				}
-			}
+			if (!index_distance)
+				index_distance = calculate_index_distance(first_b, index_desired, len);
+			if (index_distance < len / 2) 
+				rotate(&first_b, 'b');
+			else
+				reverse_rotate(&first_b, 'b');
 		}
 	}
-	// printf("Lista A:\n");
-	// print_list(first);
-	// printf("----------------:\n");
-	// printf("Lista B:\n");
-	// print_list(first_b);
+	printf("Lista A:\n");
+	print_list(first);
+	printf("----------------:\n");
+	printf("Lista B:\n");
+	print_list(first_b);
 }
